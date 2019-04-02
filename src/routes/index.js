113db9dc19092bc979c88b9b7c283b4265fcedd1
Cathy <xiaoyingchangca@gmail.com>
2019-04-01 20:46:22 -0700

    commented locus in index.js file

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const User = require("../models/user");
-const locus = require("locus");
+//const locus = require("locus");
 
 router.get("/", (req, res)=>{
     res.render("index/landing");
