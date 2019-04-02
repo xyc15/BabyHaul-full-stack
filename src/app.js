de90f4a56ed270c16e2c40aa652d0bf9cd56c686
Cathy <xiaoyingchangca@gmail.com>
2019-04-01 21:53:51 -0700

    changed databaseurl

--- src/app.js
+++ src/app.js
@@ -16,8 +16,9 @@ const productRoutes = require('./routes/products');
 const commentRoutes = require('./routes/comments');
 
 require('dotenv').config();//get environment variables
-mongoose.connect(process.env.DATABASEURL, {useFindAndModify: false, useNewUrlParser: true});
-
+//mongoose.connect(process.env.DATABASEURL, {useFindAndModify: false, useNewUrlParser: true});
+mongoose.connect(process.env.DATABASEURL, {useNewUrlParser: true});
+console.log("process.env.DATABASEURL is: ", process.env.DATABASEURL);
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
